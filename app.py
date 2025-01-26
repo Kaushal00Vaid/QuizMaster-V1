94fc4b9b19de9960f1f944b4497e44dbb688f358
Kaushal Vaid <24f2000681@ds.study.iitm.ac.in>
2025-01-26 11:15:28 +0530

    login and signup page

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('landing.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+app.run(debug=True)
